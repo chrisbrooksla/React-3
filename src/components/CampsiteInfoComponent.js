d2a2046ec03d121df3339d2961d0cbe69abe7c14
Chris Brooks <chrisbrooksla@gmail.com>
2020-12-28 17:07:23 -0600

    header_and_footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
+
  
-// renderCampsite method
-  renderCampsite(campsite) {
+// RenderCampsite component
+   function RenderCampsite({campsite}) {
     return (
 			<div className="col-md-5 m-1">
 				<Card>
@@ -18,8 +18,8 @@ class CampsiteInfo extends Component {
 		);
 }
 
-  // renderComments method
-  renderComments(comments) {
+  // RenderComments component
+  function RenderComments({comments}) {
     if (comments) {
       return (
 				<div className="col-md-5 m-1">
@@ -47,14 +47,14 @@ class CampsiteInfo extends Component {
 }
 
 
-// component render method
-  render() {
-if (this.props.campsite) {
+// CampsiteInfo component
+  function CampsiteInfo(props) {
+if (props.campsite) {
   return (
     <div className="container">
 			  <div className="row">
-					{this.renderCampsite(this.props.campsite)}
-					{this.renderComments(this.props.campsite.comments)}
+					<RenderCampsite campsite={props.campsite} />
+					<RenderComments comments= {props.campsite.comments} />
 				</div>
 		</div>
   );
@@ -66,6 +66,6 @@ if (this.props.campsite) {
   );
       }
   }
-}
+
 
 export default CampsiteInfo
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
@@ -20,11 +21,7 @@ class Main extends Component {
 	render() {
 		return (
 			<div>
-				<Navbar dark color="primary">
-					<div className="container">
-						<NavbarBrand href="/">NuCamp</NavbarBrand>
-					</div>
-				</Navbar>
+				<Header />
 				<Directory
 					campsites={this.state.campsites}
 					onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
@@ -35,7 +32,8 @@ class Main extends Component {
 							(campsite) => campsite.id === this.state.selectedCampsite
 						)[0]
 					}
-				/>
+        />
+        <Footer />
 			</div>
 		);
 	}
